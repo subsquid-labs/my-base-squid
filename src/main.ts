cbc8ce8f0e82ebc8b20134ec37682af5e9c3f066
abernatskiy <abernatskiy@toha.li>
2024-07-16 15:24:15 +0900

    Testing the FriendTech data retrieval
    	modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,27 +3,12 @@ import {Burn} from './model'
 import {processor} from './processor'
 
 processor.run(new TypeormDatabase({supportHotBlocks: true}), async (ctx) => {
-    const burns: Burn[] = []
-    for (let c of ctx.blocks) {
-        for (let tx of c.transactions) {
-            // decode and normalize the tx data
-            burns.push(
-                new Burn({
-                    id: tx.id,
-                    block: c.header.height,
-                    address: tx.from,
-                    value: tx.value,
-                    txHash: tx.hash,
-                })
-            )
+    for (let block of ctx.blocks) {
+        for (let log of block.logs) {
+            console.log(log)
         }
+        for (let tx of block.transactions) {
+            console.log(tx)
+       }
     }
-    // apply vectorized transformations and aggregations
-    const burned = burns.reduce((acc, b) => acc + b.value, 0n) / 1_000_000_000n
-    const startBlock = ctx.blocks.at(0)?.header.height
-    const endBlock = ctx.blocks.at(-1)?.header.height
-    ctx.log.info(`Burned ${burned} Gwei from ${startBlock} to ${endBlock}`)
-
-    // upsert batches of entities with batch-optimized ctx.store.save
-    await ctx.store.upsert(burns)
 })
