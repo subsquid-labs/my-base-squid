7b1a3e80ca2d6790c1166453f73b996c918f9e67
abernatskiy <abernatskiy@toha.li>
2024-07-16 16:31:44 +0900

    Testing data retrieval with decoding
    	modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import {TypeormDatabase} from '@subsquid/typeorm-store'
 import {Burn} from './model'
 import {processor} from './processor'
+import * as friendTechShares from './abi/friendTechShares'
 
 processor.run(new TypeormDatabase({supportHotBlocks: true}), async (ctx) => {
     for (let block of ctx.blocks) {
         for (let log of block.logs) {
-            console.log(log)
+            let decodedLog = friendTechShares.events.Trade.decode(log)
+            console.log(decodedLog)
         }
         for (let tx of block.transactions) {
             console.log(tx)
